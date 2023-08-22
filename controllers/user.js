997000a542879b0528faa2afdaf5197e0364207b
philipthedeveloper <philipowolabi79@gmail.com>
2023-08-22 17:29:06 +0100

    Fix cookie issue

--- controllers/user.js
+++ controllers/user.js
@@ -45,7 +45,10 @@ const createUser = async (req, res) => {
   };
   req.session.isAuth = true;
   req.session.user = userDetails;
-  res.cookie("accessToken", token);
+  res.cookie("accessToken", token, {
+    sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+    secure: process.env.NODE_ENV === "development" ? false : true,
+  });
   // res.cookie("accessToken", token, {
   //   maxAge: 1000 * 60 * 60 * 2,
   //   httpOnly: true,
@@ -102,7 +105,10 @@ const signIn = async (req, res) => {
   };
   req.session.isAuth = true;
   req.session.user = userDetails;
-  res.cookie("accessToken", token);
+  res.cookie("accessToken", token, {
+    sameSite: process.env.NODE_ENV === "development" ? "lax" : "none",
+    secure: process.env.NODE_ENV === "development" ? false : true,
+  });
   // res.cookie("accessToken", token, {
   //   maxAge: 1000 * 60 * 60 * 2,
   //   httpOnly: true,
