9ab3a8d1410277bd732035552280c913a2690398
philipthedeveloper <philipowolabi79@gmail.com>
2023-08-22 16:47:01 +0100

    Parent count added to the general route

--- controllers/general.js
+++ controllers/general.js
@@ -1,3 +1,4 @@
+const Parent = require("../models/Parent");
 const Student = require("../models/Student");
 const Teacher = require("../models/Teacher");
 const { StatusCodes } = require("http-status-codes");
@@ -5,7 +6,10 @@ const { StatusCodes } = require("http-status-codes");
 const getRecordCounts = async (req, res) => {
   const teachers = await Teacher.count();
   const students = await Student.count();
-  return res.status(StatusCodes.OK).json({ success: true, teachers, students });
+  const parents = await Parent.count();
+  return res
+    .status(StatusCodes.OK)
+    .json({ success: true, teachers, students, parents });
 };
 
 module.exports = { getRecordCounts };
